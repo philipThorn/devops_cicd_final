70b84440d9a8f65243dfb6c9bf537316451529d2
philipThorn <philip.a.torn@gmail.com>
2022-11-29 15:05:53 +0100

    test12436

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -3,7 +3,7 @@ import requests
 
 def test_product(config):
     response = requests.get(
-        config.base_url + '/product',
+        config.base_url + 'product',
         timeout=config.TIMEOUT)
     assert response.headers['content-type'] == 'application/json'
     assert response.status_code == 200
